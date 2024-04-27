041a2273a484f4d38edeeb6c9833cc6079d8606a
barpeot <akbar.putra02.ap@gmail.com>
2024-04-27 20:46:34 +0700

    initial commit

--- app/src/main/java/com/example/waterbottle/MainActivity.kt
+++ app/src/main/java/com/example/waterbottle/MainActivity.kt
@@ -3,13 +3,26 @@ package com.example.waterbottle
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.waterbottle.ui.theme.WaterBottleTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,10 +32,31 @@ class MainActivity : ComponentActivity() {
             WaterBottleTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier
+                        .fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
+                    var usedAmount by remember {
+                        mutableStateOf(400)
+                    }
+
+                    val totalWaterAmount = remember {
+                        2400
+                    }
+
+                    Column (
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ){
+                        WaterBottle(totalWaterAmt = 2400, unit = "ml", usedWaterAmt = usedAmount, modifier = Modifier.width(200.dp))
+                        Spacer(modifier = Modifier.height(20.dp))
+                        Text(text = "Total water amount is : $totalWaterAmount")
+                        Button(onClick = { usedAmount += 200 }) {
+                            Text("Drink")
+                        }
+                    }
                 }
             }
         }
